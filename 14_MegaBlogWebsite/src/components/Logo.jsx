2f4bfde32b654546bda54abf18f6982b1a4be2ed
Shivam Mittal <shivammittal.mittal.10@gmail.com>
2024-09-20 22:34:51 +0530

    done

--- 14_MegaBlogWebsite/src/components/Logo.jsx
+++ 14_MegaBlogWebsite/src/components/Logo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Logo({width = '100px'}) {
+function Logo({width='100px'}) {
   return (
-    <div>
-      Logo
+    <div className='text-white'>
+      <img src="/SB1.png" alt="logo" width={width} />
     </div>
   )
 }
